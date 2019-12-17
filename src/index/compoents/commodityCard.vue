394c7a99489e6b38f9921a599163c4ae099e7231
NoraH1to <834053207@qq.com>
2019-12-17 21:48:52 +0800

    sync

--- src/index/compoents/commodityCard.vue
+++ src/index/compoents/commodityCard.vue
@@ -1,8 +1,17 @@
 <template>
 	<div>
-		<el-card id="item-card" shadow="hover" :body-style="{ padding: '0px' }">
+		<el-card id="item-card" shadow="hover" :body-style="{ padding: '0px', margin: '0px' }">
 			<img id="item-img" :src="cItem.image_path" />
-			<div>233</div>
+			<div id="item-content">
+				<div class="text-block">
+					<span>{{ cItem.name }}</span>
+				</div>
+				<div class="text-block">
+					<span>{{ priceStringArr[0] }}</span>
+					.
+					<span>{{ priceStringArr[1] }}</span>
+				</div>
+			</div>
 		</el-card>
 	</div>
 </template>
@@ -12,14 +21,13 @@
 const defaultItem = {
 	item_id: '',
 	name: '商品名称',
-	description: '商品描述',
 	price: 233.33,
 	sales: 666,
 	image_path: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
 	shop_id: '',
-	shop_name: '店铺名称',
 	address: '产地',
-	open: 1
+	open: 1,
+	tags: ['标签一', '我是标签']
 };
 
 export default {
@@ -40,6 +48,9 @@ export default {
 		// 合并默认和传入值
 		cItem() {
 			return Object.assign(defaultItem, this.item);
+		},
+		priceStringArr() {
+			return this.cItem.price.toString().split('.');
 		}
 	}
 };
@@ -47,6 +58,12 @@ export default {
 
 <style lang="stylus" scoped>
 #item-img
-	display block
+	display blockq
 	width 100%
+
+#item-content
+	margin 0px
+
+.text-block
+	margin 5% 10%
 </style>
